bc9aec8f5a7399c69368a0a1e51ba4083d3dbacb
JH-Jitu <jitu000166@gmail.com>
2020-10-03 05:45:47 +0600

    ema-john-backend-added

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBHhOX8rtp2vahVcATkhTD_7CwARw6arrk",
-    authDomain: "fire-auth2.firebaseapp.com",
-    databaseURL: "https://fire-auth2.firebaseio.com",
-    projectId: "fire-auth2",
-    storageBucket: "fire-auth2.appspot.com",
-    messagingSenderId: "587131087182",
-    appId: "1:587131087182:web:f7e53e9007fe7760d7561f"
+    apiKey: "AIzaSyCTh4vEPYibTKdY3Vxwq6c5IUx4H4pgFLw",
+    authDomain: "earn-2018.firebaseapp.com",
+    databaseURL: "https://earn-2018.firebaseio.com",
+    projectId: "earn-2018",
+    storageBucket: "earn-2018.appspot.com",
+    messagingSenderId: "807276711190",
+    appId: "1:807276711190:web:64f0ebc0c8d91d81011437"
   };
   export default firebaseConfig;
